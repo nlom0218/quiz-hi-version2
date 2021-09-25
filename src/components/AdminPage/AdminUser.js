32cf7f42b699334c7b565b3a94a1fc35a2194be7
nlom0218 <nlom0218@naver.com>
2021-09-25 23:00:44 +0900

    User totalNum

--- src/components/AdminPage/AdminUser.js
+++ src/components/AdminPage/AdminUser.js
@@ -1,14 +1,19 @@
 import { useLazyQuery, useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import UserItem from './UserItem';
 
 const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  row-gap: 40px;
+  row-gap: 20px;
   margin-bottom: 60px;
+  .topContent {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    align-items: flex-end;
+  }
 `
 
 const SetTypeBtn = styled.div`
@@ -30,6 +35,7 @@ const Type = styled.div`
 
 const UserList = styled.ul`
   display: grid;
+  grid-template-columns: 1fr;
   background-color: rgb(200, 200, 200, 0.8);
   row-gap: 1px;
   border: 1px solid rgb(200, 200, 200, 0.8);
@@ -43,6 +49,35 @@ const UserList = styled.ul`
     column-gap: 10px;
   }
 `
+
+const PageBar = styled.div`
+  justify-self: flex-end;
+  align-self: flex-end;
+  border: 1px solid rgb(200, 200, 200, 0.6);
+  background-color: ${props => props.theme.bgColor};
+  border-radius: 5px;
+  display: flex;
+  position: relative;
+`
+
+const PageBarBtn = styled.div`
+  padding: 8px 20px;
+  transition: background-color 0.2s linear;
+  :hover {
+    background-color: rgb(200, 200, 200, 0.2);
+  }
+  :first-child {
+    border-right: 1px solid rgb(200, 200, 200, 0.6);
+    opacity: ${props => props.firstPage ? "0.4" : "1"};
+    cursor: ${props => props.firstPage ? "not-allowd" : "pointer"};
+  }
+  :nth-child(2) {
+    opacity: ${props => props.lastPage ? "0.4" : "1"};
+    cursor: ${props => props.lastPage ? "not-allowd" : "pointer"};
+  }
+`
+
+const UserTotalNum = styled.div``
 
 const ADMIN_SEE_USER_QUERY = gql`
   query adminSeeUser($type: String!, $page: Int!) {
@@ -59,19 +94,32 @@ const ADMIN_SEE_USER_QUERY = gql`
   }
 `
 
-
 const AdminUser = () => {
   const [user, setUser] = useState([])
+  const [totalNum, setTotalNum] = useState(null)
   const [type, setType] = useState(undefined)
   const [page, setPage] = useState(1)
+  const [lastPage, setLastPage] = useState(1)
   const onCompleted = (result) => {
     const { adminSeeUser: { user, totalNum } } = result
     setUser(user)
+    setTotalNum(totalNum)
+    if (totalNum === 0) {
+      setLastPage(1)
+      return
+    }
+    if (Number.isInteger(totalNum / 20)) {
+      setLastPage(totalNum / 20)
+      return
+    }
+    const lastPage = Math.floor(totalNum / 20) + 1
+    setLastPage(lastPage)
   }
   const [adminSeeUser, { loading }] = useLazyQuery(ADMIN_SEE_USER_QUERY, {
     onCompleted
   })
   const onClickType = (type) => {
+    setPage(1)
     setType(type)
     adminSeeUser({
       variables: {
@@ -79,6 +127,32 @@ const AdminUser = () => {
         page
       }
     })
+  }
+  useEffect(() => {
+    if (!type) {
+      return
+    }
+    adminSeeUser({
+      variables: {
+        type,
+        page
+      }
+    })
+  }, [page])
+  const onClickPageBtn = (btn) => {
+    if (btn === "pre") {
+      if (page === 1) {
+        return
+      } else {
+        setPage(prev => prev - 1)
+      }
+    } else if (btn === "next") {
+      if (lastPage === page) {
+        return
+      } else {
+        setPage(prev => prev + 1)
+      }
+    }
   }
   return (<Container>
     <SetTypeBtn>
@@ -99,19 +173,28 @@ const AdminUser = () => {
         onClick={() => onClickType("student")}
       >학생</Type>
     </SetTypeBtn>
-    {loading ? "loading..." :
-      <UserList>
-        <div className="sortItem">
-          <div>id</div>
-          <div>아이디</div>
-          <div>이메일</div>
-          <div>닉네임</div>
-          <div>타입</div>
+    {user.length === 0 ? "" :
+      <React.Fragment>
+        <div className="topContent">
+          <UserTotalNum>{totalNum}명의 유저</UserTotalNum>
+          <PageBar>
+            <PageBarBtn firstPage={page === 1 ? true : false} onClick={() => onClickPageBtn("pre")}>이전</PageBarBtn>
+            <PageBarBtn lastPage={lastPage === page} onClick={() => onClickPageBtn("next")}>다음</PageBarBtn>
+          </PageBar>
         </div>
-        {user.map((item, index) => {
-          return <UserItem key={index} {...item} />
-        })}
-      </UserList>
+        <UserList>
+          <div className="sortItem">
+            <div>id</div>
+            <div>아이디</div>
+            <div>이메일</div>
+            <div>닉네임</div>
+            <div>타입</div>
+          </div>
+          {user.map((item, index) => {
+            return <UserItem key={index} {...item} />
+          })}
+        </UserList>
+      </React.Fragment>
     }
   </Container>);
 }
