fb378f7584079d1fde978b2e4b73c03afe23bc8b
nlom0218 <nlom0218@naver.com>
2021-10-03 16:53:36 +0900

    Library noDataMsg

--- src/components/Library/NoDataMsg.js
+++ src/components/Library/NoDataMsg.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import useUser from '../../hooks/useUser';
 
 const Msg = styled.div`
   border-top: 1px solid rgb(200, 200, 200, 0.8);
@@ -8,8 +9,13 @@ const Msg = styled.div`
 `
 
 const NoDataMsg = ({ content }) => {
+  const user = useUser()
   return (<Msg>
-    라이브러리에 저장된 {content}가 없습니다.
+    {user?.type === "teacher" ?
+      `라이브러리에 저장된 ${content}가 없습니다.`
+      :
+      "라이브러리는 선생님 계정에서만 이용가능합니다."
+    }
   </Msg>);
 }
 
