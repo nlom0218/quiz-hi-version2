b0442afc4b373ade3d32d7b140b4dabdb241061e
nlom0218 <nlom0218@naver.com>
2021-10-09 18:15:33 +0900

    SendMsg #1

--- src/components/Home/SendMsg.js
+++ src/components/Home/SendMsg.js
@@ -1,25 +1,67 @@
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
 import useUser from '../../hooks/useUser';
-import LevelStep from '../LevelStep';
-import LinkBtn from '../LinkBtn';
 import HomeLayout from './HomeLayout';
 
 const Box = styled.div`
+  padding: 60px;
   grid-row: 2 / 3;
   background-color: rgb(77, 242, 78, 0.2);
   box-shadow: 0px 17px 6px -14px rgba(0,0,0,0.2);
   display: grid;
-  justify-items: center;
-  align-items: center;
-  font-size: 24px;
+  grid-template-columns: auto 1fr;
+  column-gap: 100px;
   margin-bottom: 40px;
+`
+
+const SendMsgInfo = styled.div`
+  font-size: 18px;
+  display: grid;
+  row-gap: 20px;
+`
+
+const Textarea = styled.textarea`
+  width: 100%;
+  resize: none;
+  border: none;
+  font-size: 16px;
+  border-radius: 5px;
+  padding: 20px 20px;
+  color: ${props => props.theme.fontColor};
+  background-color: rgb(77, 242, 78, 0.4);
+  transition: box-shadow 0.4s linear, background-color 1s ease, color 1s ease;
+  :focus {
+    box-shadow: 0 0 1px 0.5px ${props => props.theme.fontColor};
+    outline: none;
+  }
+`
+
+const MsgForm = styled.form`
+  display: grid;
+  input {
+    color: ${props => props.theme.fontColor};
+    justify-self: flex-end;
+    cursor: pointer;
+    padding: 10px 20px;
+    background-color: rgb(77, 242, 78, 0.4);
+    transition: background-color 1s ease, color 1s ease;
+    border-radius: 5px;
+    :hover {
+      background-color: rgb(77, 242, 78, 0.6);
+    }
+  }
+  row-gap: 20px;
 `
 
 const SendMsg = () => {
   const user = useUser()
+  const { register } = useForm({
+    mode: "onChange"
+  })
   return (<HomeLayout
     className="sendMsg"
     layout="sendMsg"
@@ -28,9 +70,23 @@ const SendMsg = () => {
     left={false}
   >
     <Box>
-      <div>
-        <FontAwesomeIcon icon={faEnvelope} /> 관리자에게 메시지 보내기 기능이 곧 업데이트 됩니다
-      </div>
+      <SendMsgInfo>
+        <div><FontAwesomeIcon icon={faCheck} /> 건의사항이 있으신가요?</div>
+        <div><FontAwesomeIcon icon={faCheck} /> 원하는 기능이 있으신가요?</div>
+        <div><FontAwesomeIcon icon={faCheck} /> 오류를 발견하셨나요?</div>
+        <div><FontAwesomeIcon icon={faCheck} /> 궁금하신 내용이 있으신가요?</div>
+        <div style={{ fontWeight: "600" }}>언제든지 메시지를 보내주세요!</div>
+      </SendMsgInfo>
+      <MsgForm>
+        <Textarea
+          cols={20}
+          rows={8}
+          {...register("question", {
+            required: true
+          })}
+        ></Textarea >
+        <input type="submit" value="메시지 보내기" />
+      </MsgForm>
     </Box>
   </HomeLayout>);
 }
