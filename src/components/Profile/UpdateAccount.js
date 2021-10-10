335b18049dfd302e2fb25ab27ca00809f016ea09
nlom0218 <nlom0218@naver.com>
2021-10-10 16:52:01 +0900

    UpdateAccount

--- src/components/Profile/UpdateAccount.js
+++ src/components/Profile/UpdateAccount.js
@@ -1,8 +1,193 @@
-import React from 'react';
-import { DetailInfoLayout } from './sharedCss';
+import { gql, useMutation } from '@apollo/client';
+import { faChalkboardTeacher } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane, faRedoAlt, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
+import { DetailInfoLayout, Title } from './sharedCss';
+import emailjs from 'emailjs-com';
+import dotenv from "dotenv"
+import EmailConfirm from '../Account/EmailConfirm';
+dotenv.config()
+
+const Wrapper = styled.div`
+  margin-top: 20px;
+`
+
+const Form = styled.form`
+  margin-bottom: 30px;
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  grid-template-rows: auto;
+  column-gap: 10px;
+  row-gap: 20px;
+  span {
+    grid-column: 1 / 3;
+  }
+`
+
+const Input = styled.input`
+  grid-column: 1 / 2;
+  padding: 10px 20px;
+  border-radius: 5px;
+  background-color: rgb(200, 200, 200, 0.2);
+  transition: background-color 1s ease, box-shadow 0.4s linear;
+  :focus {
+    box-shadow: 0 0 1px 0.5px ${props => props.theme.fontColor};
+  }
+`
+
+const EmailBtn = styled.button`
+  background-color: rgb(200, 200, 200);
+  opacity: ${props => props.disabled ? 0.2 : 0.9};
+  padding: 10px 20px;
+  border-radius: 5px;
+  transition: opacity 0.6s linear;
+  cursor: pointer;
+  div {
+    font-size: 12px;
+  }
+`
+
+const PlatForm = styled.div`
+  text-align: center;
+  margin-bottom: 30px;
+  a {
+    color: tomato;
+    opacity: 1;
+    margin-right: 20px;
+  }
+  svg  {
+    cursor: pointer;
+    color: tomato;
+    opacity: 0.6;
+    transition: opacity 0.3s linear;
+    :hover {
+      opacity: 1;
+    }
+  }
+`
+
+const ErrMsg = styled.div`
+  text-align: center;
+  color: tomato;
+  margin-bottom: 30px;
+  span {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`
+
+const CONFIRM_EMAIL_MUTATION = gql`
+  mutation confirmEmail($email: String!) {
+    confirmEmail(email: $email) {
+      ok
+      error
+    }
+  }
+`
 
 const UpdateAccount = () => {
-  return (<DetailInfoLayout></DetailInfoLayout>);
+  const [doneConfirm, setDoneConfirm] = useState(false)
+  const [confirmNum, setConfirmNum] = useState("")
+  const [sendEmail, setSendEmail] = useState(false)
+  const [platform, setPlatForm] = useState("")
+  const [sending, setSending] = useState(false)
+  const [errMsg, setErrMsg] = useState(null)
+  const [email, setEmail] = useState(null)
+  const { register, handleSubmit, formState: { isValid }, setValue, getValues } = useForm({
+    mode: "onChange"
+  })
+  const onCompleted = (result) => {
+    const email = getValues("email")
+    const { confirmEmail: { ok, error } } = result
+    if (ok) {
+      const randomNum = Math.floor(Math.random() * 1000000)
+      console.log(randomNum);
+      setConfirmNum(randomNum)
+      // emailjs.send(
+      //   process.env.REACT_APP_EMAILSERVICEID,
+      //   process.env.REACT_APP_EMAILTEMPLATEID,
+      //   {
+      //     email,
+      //     confirmNum: randomNum
+      //   },
+      //   process.env.REACT_APP_EMAILUSERID)
+      //   .then((result) => {
+      //     setSendEmail(true)
+      //     setSending(false)
+      //     setEmail(email)
+      //     setPlatForm(email.split("@").reverse()[0])
+      //   }, (error) => {
+      //     console.log(error.text);
+      //   })
+      setSendEmail(true)
+      setSending(false)
+      setEmail(email)
+      setPlatForm(email.split("@").reverse()[0])
+    } else {
+      setErrMsg(error)
+    }
+  }
+  const [confirmEmail, { loading }] = useMutation(CONFIRM_EMAIL_MUTATION, {
+    onCompleted
+  })
+  const onSubmit = (data) => {
+    const { email } = data
+    setSending(true)
+    if (loading) {
+      return
+    }
+    confirmEmail({
+      variables: {
+        email
+      }
+    })
+  }
+  const onClinkAgainBtn = () => {
+    setSendEmail(false)
+    setValue("email", "")
+  }
+  const onClickRetry = () => {
+    setSending(false)
+    setValue("email", "")
+    setErrMsg(null)
+  }
+  return (<DetailInfoLayout>
+    <Title>
+      <div><FontAwesomeIcon icon={faChalkboardTeacher} /> 선생님 계정으로 업데이트</div>
+    </Title>
+    <Wrapper>
+      <Form onSubmit={handleSubmit(onSubmit)} >
+        <span>이메일을 입력하세요.</span>
+        <Input
+          {...register("email", {
+            required: true,
+            pattern: /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/
+          })}
+          type="text"
+          autoComplete="off"
+          readOnly={errMsg ? "readOnly" : ""}
+        />
+        <EmailBtn type="submit" disabled={!isValid || sendEmail} >
+          {sending ? <FontAwesomeIcon icon={faSpinner} /> : <FontAwesomeIcon icon={faPaperPlane} />}
+        </EmailBtn>
+      </Form>
+      {errMsg && <ErrMsg>{errMsg} <span onClick={onClickRetry}>재시도</span></ErrMsg>}
+      {sendEmail &&
+        <React.Fragment>
+          <EmailConfirm confirmNum={confirmNum} setDoneConfirm={setDoneConfirm} />
+          <PlatForm>
+            <a href={`https://${platform}`} target="_blank">
+              {platform}로 이동하기
+            </a>
+            <FontAwesomeIcon icon={faRedoAlt} onClick={onClinkAgainBtn} />
+          </PlatForm>
+        </React.Fragment>
+      }
+    </Wrapper>
+  </DetailInfoLayout>);
 }
 
 export default UpdateAccount;
