e70f10f1b5a306c429b8cca279e0f21da7e4b1a3
nlom0218 <nlom0218@naver.com>
2021-11-05 00:10:04 +0900

    Header #2

---
+++ src/CollectionProject/components/Contents.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import { customMedia } from '../../styles';
+
+const SContents = styled.div`
+  width: 100%;
+  position: absolute;
+  top: 50px;
+  top: 3.125rem;
+  background-color: ${props => props.theme.boxColor};
+  transition: background-color 1s ease;
+  padding: 0px 30px;
+  padding: 0px 1.875rem;
+  ${customMedia.greaterThan("desktop")`
+    position: static;
+    background-color: ${props => props.theme.fontColor};
+    color: ${props => props.theme.bgColor};
+    transition: color 1s ease, background-color 1s ease;
+    padding: 0px;
+    margin-left: 30px;
+    margin-left: 1.875rem;
+  `}
+`
+
+const ContentsList = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr;
+  ${customMedia.greaterThan("tablet")`
+    grid-template-columns: 1fr 1fr;
+  `}
+  ${customMedia.greaterThan("desktop")`
+    display: flex;
+  `}
+`
+
+const ContentsItem = styled.li`
+  font-size: 0.875em;
+  font-size: 0.875rem;
+  cursor: pointer;
+  ${customMedia.greaterThan("desktop")`
+    margin-left: 30px;
+    margin-left: 1.875rem;
+  `}
+`
+
+const Contents = () => {
+  return (<SContents>
+    <ContentsList>
+      <ContentsItem>• 스피드 퀴즈</ContentsItem>
+      <ContentsItem>• 스피드 퀴즈</ContentsItem>
+      <ContentsItem>• 스피드 퀴즈</ContentsItem>
+      <ContentsItem>• 스피드 퀴즈</ContentsItem>
+    </ContentsList>
+  </SContents>);
+}
+
+export default Contents;
--- src/CollectionProject/components/Header.js
+++ src/CollectionProject/components/Header.js
@@ -1,26 +1,36 @@
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { customMedia } from '../../styles';
+import Contents from './Contents';
 
 const Container = styled.div`
   grid-column: 1 / -1;
   background-color: ${props => props.theme.fontColor};
-  padding: 20px 30px;
-  padding: 20px 1.875rem;
+  padding: 0px 30px;
+  padding: 0px 1.875rem;
+  height: 50px;
+  height: 3.125rem;
+  line-height: 50px;
+  line-height: 3.125rem;
   transition: background-color 1s ease;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  position: relative;
+  svg {
+    color: ${props => props.theme.bgColor};
+    transition: color 1s ease;
+    cursor: pointer;
+  }
   ${customMedia.greaterThan("tablet")`
-    display: flex;
+    grid-template-columns: auto auto 1fr;
   `}
 `
 
 const HomeIcon = styled.div`
-  svg {
-    color: ${props => props.theme.bgColor};
-    transition: color 1s ease;
-  }
 `
 
 const Title = styled.div`
@@ -34,10 +44,34 @@ const Title = styled.div`
   `}
 `
 
+const MenuBar = styled.div`
+  justify-self: end;
+`
+
 const Header = () => {
+  const [seeMenu, setSeeMenu] = useState(false)
+  const isDesktop = useMediaQuery({
+    query: "(min-width: 1024px)"
+  })
+  const isTablet = useMediaQuery({
+    query: "(min-width: 768px)"
+  })
+  const onClickMenu = () => { setSeeMenu(prev => !prev) }
+  const possibleMenu = () => {
+    if (isDesktop) {
+      return true
+    } else {
+      if (seeMenu) {
+        return true
+      }
+    }
+    return false
+  }
   return (<Container>
     <HomeIcon><Link to="/"><FontAwesomeIcon icon={faHome} /></Link></HomeIcon>
     <Title>QUIZ HI Collection</Title>
+    {!isDesktop && <MenuBar onClick={onClickMenu}><FontAwesomeIcon icon={faBars} /></MenuBar>}
+    {possibleMenu() && <Contents />}
   </Container>);
 }
 
