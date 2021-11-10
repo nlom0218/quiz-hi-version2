92e1fc5d21fb2395bfc21c05e0f69d1cf6ffc2b3
nlom0218 <nlom0218@naver.com>
2021-11-10 20:38:21 +0900

    Weather

--- src/CollectionProject/pages/Experiment/Experiment.js
+++ src/CollectionProject/pages/Experiment/Experiment.js
@@ -2,6 +2,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import styled from 'styled-components';
+import { FcNews, FcCalendar, FcTodoList, FcGrid } from "react-icons/fc";
 import NavBtn from './NavBtn';
 
 const Container = styled.div`
@@ -14,15 +15,28 @@ const Container = styled.div`
   background-size: cover;
   background-position: center;
   color: ${props => props.theme.fontColor};
-  transition: color 0.5s ease;
+  transition: color 1s ease;
 `
 
 const Header = styled.div`
   display: grid;
   padding: 20px;
   padding: 1.25rem;
-  justify-content: end;
+  grid-template-columns: auto 1fr auto;
+  justify-items: end;
+  align-items: flex-start;
 `
+
+const Weather = styled.div`
+  background: ${props => props.theme.bgColor};
+  transition: background 1s ease;
+  padding: 10px 20px;
+  padding: 0.625rem 1.25rem;
+  border-radius: 10px;
+  border-radius: 0.625rem;
+`
+
+const PageBtn = styled.div``
 
 const MenuBtn = styled.div`
   font-size: 20px;
@@ -43,6 +57,14 @@ const Experiment = () => {
     <Container>
       <NavBtn />
       <Header>
+        <Weather>춘천시 4˚ 흐림 미세먼지 좋음
+        </Weather>
+        <PageBtn>
+          <FcNews />
+          <FcTodoList />
+          <FcCalendar />
+          <FcGrid />
+        </PageBtn>
         <MenuBtn><FontAwesomeIcon icon={faBars} /></MenuBtn>
       </Header>
       <Content>
